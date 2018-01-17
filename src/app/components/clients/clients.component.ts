1c0427cea9c18455f78ae66f1202460537960d37
Seif <seif.eleslam1990@gmail.com>
2018-01-17 15:48:43 +0200

    get clients form serivce in client component

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ClientService } from '../../services/client.service';
+import { Client } from '../../models/client';
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
   styleUrls: ['./clients.component.css']
 })
 export class ClientsComponent implements OnInit {
-
-  constructor() { }
+  clients: Client[];
+  constructor(
+    public clientService: ClientService
+  ) { }
 
   ngOnInit() {
+    this.clientService.getClients().subscribe(clients => {this.clients = clients;
+    console.log(this.clients)
+    })
   }
 
 }
--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable };
+import { Observable } from 'rxjs';
+import { Client } from '../models/client';
 
 @Injectable()
 export class ClientService {
+  clients: FirebaseListObservable<any[]>;
+  client: FirebaseObjectObservable<any>
 
-  constructor() { }
+  constructor(public af: AngularFireDatabase) {
+    this.clients = this.af.list('/clients') as FirebaseListObservable<Clinet[]>;
+   }
+
+   getClients(){
+     return this.clients;
+   }
 
 }
